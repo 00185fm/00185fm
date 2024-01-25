aed29ac6a8b89de10fe01a26cb4a0d1c943cbb20
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-01-25 15:55:32 +0100

    feat: light/dark selector

--- backend/admin/src/lib/components/layout/heading.svelte
+++ backend/admin/src/lib/components/layout/heading.svelte
@@ -4,18 +4,20 @@
 	import { enhance } from '$app/forms';
 	export let currentUser: any;
 	export let page: string;
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
 <AppBar>
 	<svelte:fragment slot="lead">
 		<a href="/">
-			<img src="/img/00185fm_logo200px.png" alt="00185fm Logo" />
+			<img class="dark:invert" src="/img/00185fm_logo200px.png" alt="00185fm Logo" />
 		</a>
 	</svelte:fragment>
 	{#if currentUser}
 		<Breadcrumb {page} />
 	{/if}
 	<svelte:fragment slot="trail">
+		<LightSwitch />
 		{#if currentUser}
 			<form method="POST" action="/logout" use:enhance>
 				<button type="submit" class="btn variant-filled">Logout</button>
