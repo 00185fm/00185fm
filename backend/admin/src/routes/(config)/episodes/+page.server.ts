55d8f1d4c7d98b2f29ba242218f8cceaec12cfbc
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-01-25 16:00:35 +0100

    feat: episode card size selector

--- backend/admin/src/routes/(config)/episodes/+page.server.ts
+++ backend/admin/src/routes/(config)/episodes/+page.server.ts
@@ -1,18 +1,12 @@
-import { pb } from "$lib/pocketbase";
-import { Collections } from "$lib/pocketbase/types";
-import { redirect } from "@sveltejs/kit";
-
-/** @type {import('@sveltejs/kit').Load} */
-export const load = async ({ locals }) => {
-  if (!locals?.user) throw redirect(302, "/login");
-  try {
-    const episodes = structuredClone(
-      await pb
-        .collection(Collections.Episodes)
-        .getFullList({ sort: "-date", expand: "show" })
-    );
-    return { episodes };
-  } catch (error) {
-    console.log(error);
-  }
+import { pb } from '$lib/pocketbase';
+import { Collections } from '$lib/pocketbase/types';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import type { RecordModel } from 'pocketbase';
+export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals?.user) throw redirect(302, '/login');
+	const episodes: RecordModel[] = structuredClone(
+		await pb.collection(Collections.Episodes).getFullList({ sort: '-date', expand: 'show' })
+	);
+	return { episodes };
 };
--- backend/admin/src/routes/(config)/episodes/+page.svelte
+++ backend/admin/src/routes/(config)/episodes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import GalleryThumbnails from '$lib/icons/gallery-thumbnails.svelte';
+	import AlignJustify from '$lib/icons/align-justify.svelte';
+
 	import EpisodeCard from '$lib/components/cards/episodeCard.svelte';
-	import { SlideToggle } from '@skeletonlabs/skeleton';
+	import { RadioGroup, RadioItem, SlideToggle } from '@skeletonlabs/skeleton';
 	import { checkEmpty, playlistsToString, tagsToString } from '$lib/client/utility';
 	export let data;
 	const { episodes } = data;
 	let showIncomplete = false;
-	const cardDimension = 1;
+	let cardDimension = 0;
 
 	import { createSearchStore, searchHandler } from '$lib/stores/search';
 	import { onDestroy } from 'svelte';
@@ -36,10 +39,29 @@
 				placeholder="Search..."
 				bind:value={$searchStore.search}
 			/>
-
-			<SlideToggle class="p-8" size="sm" name="incomplete" bind:checked={showIncomplete}
+			<div class="p-5 sm:float-right">
+				<RadioGroup
+					class="w-full"
+					active="variant-ghost-primary"
+					hover="hover:variant-soft-primary"
+					padding="px-2 py-0"
+				>
+					<span>View</span>
+					<RadioItem bind:group={cardDimension} name="dimension" value={0}
+						><AlignJustify /></RadioItem
+					>
+					<RadioItem bind:group={cardDimension} name="dimension" value={1}
+						><GalleryThumbnails /></RadioItem
+					>
+				</RadioGroup>
+				<div class="pt-2 w-full">
+					<span class="align-top">⛔️ Only</span>
+					<SlideToggle size="sm" name="incomplete" bind:checked={showIncomplete} />
+				</div>
+			</div>
+			<!-- <SlideToggle class="p-8" size="sm" name="incomplete" bind:checked={showIncomplete}
 				>Incomplete</SlideToggle
-			>
+			> -->
 		</div>
 		<div class="grid grid-rows-1 sm:grid-cols-5 gap-2">
 			{#each $searchStore.filtered as episode}
