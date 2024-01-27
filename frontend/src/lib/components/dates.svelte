e8699980d696d0abe9429d37babd1e419b673d73
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-01-27 18:24:43 +0100

    feat: componentization

---
+++ frontend/src/lib/components/dates.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import type { RecordModel } from 'pocketbase';
+	export let dates: RecordModel[];
+	import Chip from './chip.svelte';
+</script>
+
+<div
+	class="no-scrollbar order-first overflow-x-scroll p-4 lg:order-last lg:w-[50%] lg:overflow-x-scroll"
+>
+	<div class="grid w-max grid-cols-4 gap-8">
+		{#each dates as year}
+			<div class="grid grid-flow-col gap-2.5 lg:grid-flow-row">
+				<span class="text-stroke font-basteleur text-2xl">{year.year}</span>
+				{#each year.months as month}
+					<Chip
+						text={month.month}
+						outline="outline-dashed outline-1 outline-black"
+						padding="p-2 justify-center"
+						size="w-fit"
+						c_class="text-xl"
+						bg_color="transparent"
+						font="font-basteleur"
+					/>
+				{/each}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<!-- 
+<div
+	class="no-scrollbar order-first overflow-x-scroll p-4 lg:order-last lg:w-[50%] lg:overflow-x-scroll"
+>
+	<div class="grid w-max grid-cols-4 gap-8">
+		{#each { length: 4 } as _, year}
+			<div class="grid grid-flow-col gap-2.5 lg:grid-flow-row">
+				<span class="text-stroke font-basteleur text-2xl">{'202' + (year + 1)}</span>
+				{#each { length: 12 } as _, month}
+					<Chip
+						text={'jan' + (month + 1)}
+						outline="outline-dashed outline-1 outline-black"
+						padding="p-2 justify-center"
+						size="w-fit"
+						c_class="text-xl"
+						bg_color="transparent"
+						font="font-basteleur"
+					/>
+				{/each}
+			</div>
+		{/each}
+	</div>
+</div> -->
---
+++ frontend/src/lib/components/tags.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { RecordModel } from 'pocketbase';
+	import Chip from './chip.svelte';
+
+	export let tags: RecordModel[];
+</script>
+
+<div class="no-scrollbar h-28 overflow-x-scroll p-4 lg:h-full lg:w-[50%] lg:overflow-y-scroll">
+	{#each tags as tag}
+		<Chip text={tag.tag} padding="px-2 m-2 mt-1" c_class="uppercase text-xs" />
+	{/each}
+</div>
