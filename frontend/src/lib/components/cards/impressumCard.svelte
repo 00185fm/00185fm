6217660b27b2a39c984b4920cd2ecdb5e33aff99
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-01-27 18:24:21 +0100

    feat: db data + styling

---
+++ frontend/src/lib/components/cards/impressumCard.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let impressum: string;
+</script>
+
+<p class="absolute -bottom-2 -right-72 w-fit bg-white font-basteleur">
+	{@html impressum}
+</p>
---
+++ frontend/src/lib/icons/logos/fb_logo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let size = 25;
+</script>
+
+<!-- viewBox="-8 -3 32 32" -->
+<svg
+	fill="#000000"
+	width={size}
+	height={size}
+	version="1.1"
+	viewBox="0 0 20 28"
+	xmlns="http://www.w3.org/2000/svg"
+	><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+		id="SVGRepo_tracerCarrier"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	></g><g id="SVGRepo_iconCarrier">
+		<title>facebook</title>
+		<path
+			d="M1.188 5.594h18.438c0.625 0 1.188 0.563 1.188 1.188v18.438c0 0.625-0.563 1.188-1.188 1.188h-18.438c-0.625 0-1.188-0.563-1.188-1.188v-18.438c0-0.625 0.563-1.188 1.188-1.188zM14.781 17.281h2.875l0.125-2.75h-3v-2.031c0-0.781 0.156-1.219 1.156-1.219h1.75l0.063-2.563s-0.781-0.125-1.906-0.125c-2.75 0-3.969 1.719-3.969 3.563v2.375h-2.031v2.75h2.031v7.625h2.906v-7.625z"
+		></path>
+	</g></svg
+>
