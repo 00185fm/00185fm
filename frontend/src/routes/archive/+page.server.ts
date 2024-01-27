cd451b7fe8a9caa4fa4180f0ba5d1fd6b861773d
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-01-27 18:25:59 +0100

    feat: db data

--- frontend/src/routes/archive/+page.server.ts
+++ frontend/src/routes/archive/+page.server.ts
@@ -1,7 +1,9 @@
 import type { PageServerLoad } from './$types';
 import type { ListResult, RecordModel } from 'pocketbase';
-import { fetchEpisodes_paginated } from '$lib/utility/utility';
+import { fetchAllRecords, fetchEpisodes_paginated } from '$lib/utility/utility';
 export const load: PageServerLoad = async () => {
+	let dates: RecordModel[] = [];
+	let tags: RecordModel[] = [];
 	let init_episodes: ListResult<RecordModel> = {
 		page: 1,
 		perPage: 60,
@@ -10,9 +12,11 @@ export const load: PageServerLoad = async () => {
 		items: []
 	};
 	try {
+		tags = await fetchAllRecords('tag_episodes', '-count');
+		dates = await fetchAllRecords('date_episodes', '-year');
 		init_episodes = structuredClone(await fetchEpisodes_paginated(1));
 	} catch (error) {
 		console.log(error);
 	}
-	return { init_episodes };
+	return { init_episodes, tags, dates };
 };
