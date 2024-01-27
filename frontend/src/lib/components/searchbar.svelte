36060a9773e7ca13c4c1f638dcaf4df7248a756e
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-01-27 18:25:00 +0100

    fix: positioning

--- frontend/src/lib/components/searchbar.svelte
+++ frontend/src/lib/components/searchbar.svelte
@@ -1,12 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let img: HTMLImageElement;
+	let width = 0;
+	let height = 0;
+	onMount(() => {
+		width = img.width;
+		height = img.height;
+	});
+</script>
+
 <div class="relative h-12">
-	<img class="border-img" src="/img/search_box.svg" alt="Search Box" />
-	<div class="flex h-full items-center justify-center">
-		<img class="w-6 pl-1" src="/img/search_icon.svg" alt="Search Icon" />
+	<img bind:this={img} class="border-img" src="/img/search_box.svg" alt="Search Box" />
+	<div class="flex h-full items-center justify-center pl-4">
+		<img class="inline-block" src="/img/search_icon.svg" alt="Search Icon" />
 		<input
 			id="searchbox"
 			name="searchbox"
 			type="text"
-			class="searchbox font-basteleur h-full w-2/3 pl-2"
+			class="searchbox inline-block h-full max-w-44 pl-2 font-basteleur"
 			placeholder="Search.."
 		/>
 	</div>
